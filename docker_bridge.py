9c89fa2ad24c649c176bf49b1cdbc70e29b39c1d
Clicks <58398364+CuzImClicks@users.noreply.github.com>
2022-09-12 16:36:19 +0000

    added more documentation to bridge

--- docker_bridge.py
+++ docker_bridge.py
@@ -9,6 +9,9 @@ def cmd(command):
 
     return subprocess.check_output(command).decode()
 
+def system(command):
+    return os.system(str(command))
+
 def get_container_id():
     return cmd("docker ps").split("\n")[1][:12]
 
@@ -26,11 +29,30 @@ def is_int(number):
     except:
         return False
 
+print("_"*20)
+print(f"CuzImClicks/Raccoon Docker Bridge")
+print("\n")
+
+print("""Commands
+upload <number|filepath>
+download <number|filepath>
+mv <file> <new_loc>
+printenv
+id
+exit
+""")
+
 while True:
     argv = input("docker bridge >>> ")
     argv = argv.split(" ")
     first = argv.pop(0)
     if first == "upload":
+        if not len(argv) == 1:
+            print("Usage")
+            print("upload <number|filepath>")
+            print("    number - The number of the image (image<number>.jpg)")
+            print("    filepath - The path to the image")
+            continue
         for file in argv:
             if is_int(file):
                 file = f"image{file}.jpg"
@@ -43,6 +65,12 @@ while True:
             time.sleep(2)
 
     elif first == "download":
+        if not len(argv) == 1:
+            print("Usage")
+            print("download <number|filepath>")
+            print("    number - The number of the image (image<number>.jpg)")
+            print("    filepath - The path to the image")
+            continue
         for file in argv:
             if is_int(file):
                 file = f"new_image{file}.jpg"
@@ -54,17 +82,41 @@ while True:
             time.sleep(2)
 
     elif first == "mv":
+        if not len(argv) == 2:
+            print("mv <file> <new_loc>")
+            print("    file - The current location of the file")
+            print("    new_loc - New location of the file")
+            continue
+
         os.replace(f"./{argv[1]}", f"./{argv[2]}")
         print(f"Moved file ./{argv[1]} ./{argv[2]}")
 
     elif first == "env":
-        os.environ[str(argv[0])] = str(argv[1])
+        print("Not working")
+        continue
+        if not len(argv) == 2:
+            print("env <name> <value>")
+            print("    name - The name of the environment variable")
+            print("    value - The value of the environment variable")
+            continue
+
+        system(f'export {str(argv[0])}="{str(argv[1])}"')
         print(f"Set env {str(argv[0])} = {str(argv[1])}")
 
     elif first == "printenv":
         if len(argv) > 0:
-            print(os.environ[argv[0]])
+            environ_dict = os.environ
+            arg = argv[0]
+            if arg in environ_dict.keys():
+                print(f"{arg} = {environ_dict[arg]}")
+            else:
+                print(f"The environment variables do not contain {arg}")
+                
+            continue
         print(os.environ)
+
+    elif first == "id":
+        print(container_id)
 
     elif first == "exit":
         exit()
