dc1fd2a4e8046ec699a9b025339effe06eaa0568
CuzImClicks <58398364+CuzImClicks@users.noreply.github.com>
2022-12-05 01:16:19 +0100

    made push and pull smarter

---
+++ pull.py
@@ -0,0 +1,12 @@
+
+import argparse
+import os
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("source", help="Copy the SOURCE to the destination", type=str)
+    parser.add_argument("destination", help="Copy the source to the DESTINATION", type=str)
+    parser.add_argument("-u", "--user", help="User for login", type=str, default="fabian", required=False)
+    parser.add_argument("-ip", "--ip", help="The IP for the SCP", type=str, default="192.168.240.200", required=False)
+    args = parser.parse_args()
+    os.system(f"scp {args.user}@{args.ip}:{args.source if args.source != '' else '/share/Henrik/input/*.jpg'} {args.destination if args.destination != '' else 'input/'}")
