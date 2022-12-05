add696fcfc83b2377f86d615536c954bfc6d479e
CuzImClicks <58398364+CuzImClicks@users.noreply.github.com>
2022-12-05 01:25:23 +0100

    fixed typo

--- push.py
+++ push.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     parser.add_argument("-u", "--user", help="User for login", type=str, default="fabian", required=False)
     parser.add_argument("-ip", "--ip", help="The IP for the SCP", type=str, default="192.168.240.200", required=False)
     args = parser.parse_args()
-    os.system(f"scp {args.source if args.source != '' else 'output/*.jpg'}{args.user}@{args.ip}:{args.destination if args.destination != '' else '/share/Henrik/output/'}")
+    os.system(f"scp {args.source if args.source != '' else 'output/*.jpg'} {args.user}@{args.ip}:{args.destination if args.destination != '' else '/share/Henrik/output/'}")
