97e3e33ec96afb59c8cd5824371dd9089e2511ba
CuzImClicks <58398364+CuzImClicks@users.noreply.github.com>
2022-12-06 23:46:16 +0100

    made source and destination non required

--- pull.py
+++ pull.py
@@ -4,8 +4,8 @@ import os
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("source", help="Copy the SOURCE to the destination", type=str)
-    parser.add_argument("destination", help="Copy the source to the DESTINATION", type=str)
+    parser.add_argument("source", help="Copy the SOURCE to the destination", type=str, required=False)
+    parser.add_argument("destination", help="Copy the source to the DESTINATION", type=str, required=False)
     parser.add_argument("-u", "--user", help="User for login", type=str, default="fabian", required=False)
     parser.add_argument("-ip", "--ip", help="The IP for the SCP", type=str, default="192.168.240.200", required=False)
     args = parser.parse_args()
