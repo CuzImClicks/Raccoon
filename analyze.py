f5dfbc6e1be3c5c88d183787c2d6bbdbc4974001
Clicks <58398364+CuzImClicks@users.noreply.github.com>
2023-01-23 14:53:27 +0100

    hopefully fixed

--- analyze.py
+++ analyze.py
@@ -12,38 +12,34 @@ parser.add_argument("--lowest", action="store_true", required=False)
 
 args = parser.parse_args()
 
-line_regex = r"Epoch (?P<epoch>\d+)\/\d+\n\d+\/\d+ \[=+\] - (?P<time>\d+)s (?P<time_per_step>\d+)ms\/step - det_loss: (?P<det_loss>\d+\.\d+) - cls_loss: (?P<cls_loss>\d+\.\d+) - box_loss: (?P<box_loss>\d+\.\d+) - reg_l2_loss: (?P<reg_l2_loss>\d+\.\d+) - loss: (?P<loss>\d+\.\d+) - learning_rate: (?P<learning_rate>\d+\.\d+(e-\d+)?) - gradient_norm: (?P<gradient_norm>\d+\.\d+)"
+line_regex = r"Epoch (?P<epoch>\d+)\/\d+\n\d+\/\d+ \[=+\] - (?P<time>\d+)s (?P<time_per_step>\d+)ms\/step - det_loss: (?P<det_loss>\d+\.\d+) - cls_loss: (?P<cls_loss>\d+\.\d+) - box_loss: (?P<box_loss>\d+\.\d+) - reg_l2_loss: (?P<reg_l2_loss>\d+\.\d+) - loss: (?P<loss>\d+\.\d+) - learning_rate: (?P<learning_rate>\d+\.\d+(e-\d+)?) - gradient_norm: (?P<gradient_norm>\d+\.\d+)\n"
 
 line_regex_val = r"Epoch (?P<epoch>\d+)\/\d+\n\d+\/\d+ \[=+\] - (?P<time>\d+)m?s? (?P<time_per_step>\d+)m?s?\/step - det_loss: (?P<det_loss>\d+.\d+) - cls_loss: (?P<cls_loss>\d+.\d+) - box_loss: (?P<box_loss>\d+.\d+) - reg_l2_loss: (?P<reg_l2_loss>\d+.\d+) - loss: (?P<loss>\d+.\d+) - learning_rate: (?P<learning_rate>\d+.\d+(e-\d+)?) - gradient_norm: (?P<gradient_norm>\d+.\d+) - val_det_loss: (?P<val_det_loss>\d+.\d+) - val_cls_loss: (?P<val_cls_loss>\d+.\d+) - val_box_loss: (?P<val_box_loss>\d+.\d+) - val_reg_l2_loss: (?P<val_reg_l2_loss>\d+.\d+) - val_loss: (?P<val_loss>\d+.\d+)\n"
 
 def generate_data(data: str):
-    return [{ "epoch": int(match[0]), "time": int(match[1]), "time_per_step": int(match[2]), "det_loss": float(match[3]), "cls_loss": float(match[4]), "box_loss": float(match[5]), "reg_l2_loss": float(match[6]), "loss": float(match[7]), "learning_rate": float(match[8]), "gradient_norm": float(match[10]) } for match in  re.findall(line_regex_val, data)]
+    return [{ "epoch": int(match[0]), "time": int(match[1]), "time_per_step": int(match[2]), "det_loss": float(match[3]), "cls_loss": float(match[4]), "box_loss": float(match[5]), "reg_l2_loss": float(match[6]), "loss": float(match[7]), "learning_rate": float(match[8]), "gradient_norm": float(match[10]) } for match in re.findall(line_regex, data)]
 
 
 def generate_data_val(data: str):
     return [{ "epoch": int(match[0]), "time": int(match[1]), "time_per_step": int(match[2]), "det_loss": float(match[3]), "cls_loss": float(match[4]), "box_loss": float(match[5]), "reg_l2_loss": float(match[6]), "loss": float(match[7]), "learning_rate": float(match[8]), "gradient_norm": float(match[10]), "val_det_loss": float(match[11]), "val_cls_loss": float(match[12]), "val_box_loss": float(match[13]), "val_reg_l2_loss": float(match[14]), "val_loss": float(match[15]) } for match in re.findall(line_regex_val, data)]
 
-
 files = []
 for f in args.input:
     if os.path.isfile(f):
-        files.append(open(f, "r").read())
+        files.append(f)
     else:
-        files.extend([open(f"{f}/{file}", "r").read() for file in os.listdir(f)])
-files = [generate_data(file) if not "val_loss" in file else generate_data_val(file) for file in files]
+        files.extend([f"{f}/{file}" for file in os.listdir(f)])
+files = [generate_data_val(open(file, "r").read()) if "val_loss" in open(file, "r").read() else generate_data(open(file, "r").read()) for file in files]
 
 import matplotlib.pyplot as plt
 
 def add_graph(plt, data: dict, name: str, draw_end=False, draw_lowest=False):
-    if any([name not in match for match in data]):
-        return
     values = [float(match[name]) for match in data]
     plt.plot(range(1,  int(data[-1]["epoch"]) + 1), values, label = name)
     if draw_end:
         last = values[-1]
         plt.axhline(y=last)
         plt.annotate(text=str(last), xy=(values.index(last), last) )
-    
     if draw_lowest:
         lowest = min(values)
         lowest_x = values.index(lowest)
@@ -52,7 +48,8 @@ def add_graph(plt, data: dict, name: str, draw_end=False, draw_lowest=False):
 
 for metric in args.metrics:
     for file in files:
-        add_graph(plt, file, metric, args.end, args.lowest)
+        if metric in file[0].keys():
+            add_graph(plt, file, metric, args.end, args.lowest)
 
 plt.legend(loc="upper right")
 plt.xlabel("Epoch")
